14146d9e7b09e9e878a1b445845e9241c6354bdc
agoyal <aditya.g@incubyte.co>
2024-05-20 10:08:30 +0530

    Added abstract class Piece to represent a chess piece

---
+++ src/main/java/org/example/domain/Piece.java
@@ -0,0 +1,14 @@
+package org.example.domain;
+
+import java.util.List;
+
+public abstract class Piece {
+
+  protected Cell position;
+
+  protected Piece(Cell position) {
+    this.position = position;
+  }
+
+  public abstract List<Cell> getAllPossibleMoves();
+}
